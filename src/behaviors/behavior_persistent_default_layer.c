b69aa850f7e6d7462d72b248897a01ffc926bbab
zael99 <bdinelt@gmail.com>
2026-04-17 16:00:05 -0400

    Update behavior_persistent_default_layer.c

--- src/behaviors/behavior_persistent_default_layer.c
+++ src/behaviors/behavior_persistent_default_layer.c
@@ -13,13 +13,13 @@
 #include <string.h>
 #include <zmk/behavior.h>
 #include <zmk/keymap.h>
-#include <zmk/persistent_layer.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-#include <zephyr/settings/settings.h>
+#include <zmk/persistent_layer.h>
 
 #if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_PDF)
+
 struct behavior_persistent_default_layer_config {
     uint8_t default_layer;
 };
@@ -27,10 +27,11 @@ struct behavior_persistent_default_layer_config {
 /* ====== Key Binding Handlers ====== */
 static int pdf_binding_pressed(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event) {
     // Switch to the specified layer
-    zmk_keymap_layer_to(binding->param1);
+    zmk_keymap_layer_id_t layerId = zmk_keymap_layer_index_to_id(binding->param1);
+    zmk_keymap_layer_to(layerId);
     
     // Save the layer to persistent storage
-    pdf_settings_save(binding->param1);
+    pdf_settings_save(layerId);
 
     return ZMK_BEHAVIOR_OPAQUE;
 }
